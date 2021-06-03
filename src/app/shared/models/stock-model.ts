a7a6bb105cd9ed001a208f8f0606f14f41509770
RomuloFS <romulosousa865@gmail.com>
2021-06-03 12:47:34 -0300

    homebroker-front-end

---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,14 @@
+class Stock{
+  // sempre com a primeira letra dos tipos minuscula
+  id: number;
+  name: string;
+  price: number;
+  minimum: number;
+  exclusiveMinimum: boolean;
+  variation: number;
+  date: string;
+  //list: string[]; // Array
+  //stock: stock;
+}
+
+export default Stock;
